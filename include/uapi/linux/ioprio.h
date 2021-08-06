58fbb43a5bd4dc2213aed454951f4c6e5fd72b0e
Piotr Gorski <lucjan.lucjanov@gmail.com>
2021-08-06 11:41:29 +0200

    Revert "block: rename IOPRIO_BE_NR"
    This reverts commit 96c879fb64303d6d06e64cd63487941159bec5a1.
    
    Signed-off-by: Piotr Gorski <lucjan.lucjanov@gmail.com>

--- include/uapi/linux/ioprio.h
+++ include/uapi/linux/ioprio.h
@@ -37,9 +37,9 @@ static inline bool ioprio_valid(unsigned short ioprio)
 }
 
 /*
- * The RT an BE priority classes support up to 8 priority levels.
+ * 8 best effort priority levels are supported
  */
-#define IOPRIO_NR_LEVELS	(8)
+#define IOPRIO_BE_NR	(8)
 
 enum {
 	IOPRIO_WHO_PROCESS = 1,
