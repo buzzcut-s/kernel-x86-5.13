94951082a0568ee3fc5ab3a1ef0da5f7810f7b6e
Piotr Gorski <lucjan.lucjanov@gmail.com>
2021-08-11 11:35:48 +0200

    Revert "bfq: optimize the calculation of bfq_weight_to_ioprio()"
    This reverts commit 9e000636e42135c611ad36c5d80fa1fd6fd18cf4.
    
    Signed-off-by: Piotr Gorski <lucjan.lucjanov@gmail.com>

--- block/bfq-wf2q.c
+++ block/bfq-wf2q.c
@@ -527,9 +527,8 @@ unsigned short bfq_ioprio_to_weight(int ioprio)
  */
 static unsigned short bfq_weight_to_ioprio(int weight)
 {
-	int ioprio = IOPRIO_NR_LEVELS  - weight / BFQ_WEIGHT_CONVERSION_COEFF;
-
-	return ioprio < 0 ? 0 : min_t(int, ioprio, IOPRIO_NR_LEVELS - 1);
+	return max_t(int, 0,
+		     IOPRIO_NR_LEVELS * BFQ_WEIGHT_CONVERSION_COEFF - weight);
 }
 
 static void bfq_get_entity(struct bfq_entity *entity)
